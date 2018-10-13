9cb1ecc659276df0e47d9f4da5a06e47e6d2d4ba
Yuuki Kanasugi <knsg16@gmail.com>
2018-10-13 20:47:23 +0900

    ブログ登録時にスクレイピング完了

--- app/models/article.rb
+++ app/models/article.rb
@@ -5,16 +5,25 @@ class Article < ApplicationRecord
   has_many :universities, through: :article_universities
   has_many :article_categories
   has_many :categories, through: :article_categories
-  enum university: { すべての大学:0, ウプサラ大学: 1, ストックホルム大学: 2, 大学タグ無し: 100}
-  enum category: { すべてのカテゴリー:0, 持物:1, 準備:2, 授業:3,
-                   寮:4, 体験談:5, 文化:6, 生活:7, カテゴリータグ無し: 100}
+  accepts_nested_attributes_for :article_universities, allow_destroy: true
+  accepts_nested_attributes_for :article_categories, allow_destroy: true
+
+  enum university: { すべての大学:0, ウプサラ大学: 3, ストックホルム大学: 2, 大学タグ無し: 1}
+  enum category: { すべてのカテゴリー:0, 持物:8, 準備:2, 授業:3,
+                   寮:4, 体験談:5, 文化:6, 生活:7, カテゴリータグ無し: 1}
+
   scope :get_by_university, ->(university) {
-    joins(:categories, :universities, :blog)
-    .where("universities.id = ?", university)
+    self.joins(:categories, :universities, :blog)
+        .where("universities.id = ?", university)
   }
   scope :get_by_category, ->(category) {
-    joins(:categories, :universities, :blog)
-    .where("categories.id = ?", category)
+    self.joins(:categories, :universities, :blog)
+        .where("categories.id = ?", category)
   }
-
+  scope :join_all, ->{
+    self.left_joins(:categories, :universities, :blog)
+        .select("articles.*, categories.category_name,
+                 (universities.name) AS univ_name, blogs.blog_name")
+        .order(id: :asc)
+  }
 end
