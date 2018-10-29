7eaef9c5963b98e182e0d645c743ec541a3bfa40
Yuuki Kanasugi <knsg16@gmail.com>
2018-10-30 08:52:00 +0900

    deleteフラグ追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_07_24_003951) do
+ActiveRecord::Schema.define(version: 2018_10_29_233105) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
@@ -59,6 +59,7 @@ ActiveRecord::Schema.define(version: 2018_07_24_003951) do
     t.integer "blog_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.boolean "is_delete", default: false, null: false
   end
 
   create_table "blogs", force: :cascade do |t|
